4d9e9b32f37798ff1852d6c0eb7f4b11d3c92dbb
Clément Pit--Claudel <clement.pitclaudel@live.com>
2016-05-17 15:24:27 -0400

    Rename domain file

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -31,7 +31,7 @@ sys.path.append(os.path.abspath('../utils/python/'))
 # ones.
 extensions = [
     'sphinx.ext.mathjax',
-    'coqrst.sphinx'
+    'coqrst.coqdomain'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
