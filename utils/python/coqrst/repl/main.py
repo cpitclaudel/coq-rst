3a76a3b435051d41e5f0901b7bc49f28d23bb251
Clément Pit--Claudel <clement.pitclaudel@live.com>
2016-05-19 23:09:48 -0400

    Suppress pexpect's send delay in repl module

--- utils/python/coqrst/repl/main.py
+++ utils/python/coqrst/repl/main.py
@@ -2,6 +2,8 @@ import re
 import pexpect
 
 class CoqTop:
+    COQTOP_PROMPT = re.compile("\r\n[^< ]+ < ")
+
     def __init__(self, coqtop_bin="coqtop", color=False, args=None):
         self.coqtop_bin = coqtop_bin
         self.args = (args or [])
@@ -13,6 +15,8 @@ class CoqTop:
         if self.coqtop:
             raise ValueError("Nested Coqtop contexts")
         self.coqtop = pexpect.spawn(self.coqtop_bin, args=self.args, echo=False, encoding="utf-8")
+        # Disable delays (http://pexpect.readthedocs.io/en/stable/commonissues.html?highlight=delaybeforesend)
+        self.coqtop.delaybeforesend = 0
         self.next_prompt()
         return self
 
@@ -20,15 +24,17 @@ class CoqTop:
         self.coqtop.kill(9)
 
     def next_prompt(self):
-        COQTOP_PROMPT = "\r\n[^< ]+ < "
-        self.coqtop.expect(COQTOP_PROMPT, timeout = 1)
+        self.coqtop.expect(CoqTop.COQTOP_PROMPT, timeout = 1)
         return self.coqtop.before
 
     def sendline(self, line):
-        line = re.sub(r"\s+", " ", line).strip()
-        print("Sending {}".format(line))
+        # Suppress newlines, but not spaces: they are significant in notations
+        line = re.sub(r"[\r\n]+", " ", line).strip()
+        # print("Sending {}".format(line))
         self.coqtop.sendline(line)
-        return self.next_prompt()
+        output = self.next_prompt()
+        # print("Got {}".format(repr(output)))
+        return output
 
 def sendlines(*lines):
     with CoqTop() as coqtop:
@@ -41,8 +47,11 @@ def sendlines(*lines):
 
 # Process the document, and then do another pass on it to add the output.
 def main():
-    sendlines("Goal False -> True.", "Proof.", "intros H.",
-              "Check H.", "Chchc.", "apply I.", "Qed.")
+    with CoqTop() as coqtop:
+        for _ in range(20):
+            print(repr(coqtop.sendline("Check nat.")))
+    # sendlines("Goal False -> True.", "Proof.", "intros H.",
+    #           "Check H.", "Chchc.", "apply I.", "Qed.")
 
 if __name__ == '__main__':
     main()
